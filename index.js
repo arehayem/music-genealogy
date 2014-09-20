1dd72f37fc42c518d38907b69529a3655ffca4ff
Andrew Rehayem <andrewrehayem@gmail.com>
2014-09-20 15:09:49 -0400

    added results route

--- index.js
+++ index.js
@@ -5,5 +5,6 @@ var requestHandlers = require("./requestHandlers")
 var handle = {};
 handle["/"] = requestHandlers.search;
 handle["/search"] = requestHandlers.search;
+handle["/results"] = requestHandlers.results;
 
 app.start(router.route, handle);
