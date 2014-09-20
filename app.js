d272b0bec816bfc8ecd1057068885e3a1292ddfa
Andrew Rehayem <andrewrehayem@gmail.com>
2014-09-20 13:33:34 -0400

    added a router

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var http = require("http");
 var url = require("url");
-var querystring = require("querystring");
 
-function onRequest(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
+function start(route, handle) {
+  function onRequest(request, response) {
+    var pathname = url.parse(request.url).pathname;
+    console.log("Request for " + pathname + " received.");
+
+    route(handle, pathname, response);
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log("Server has started.");
 }
 
-http.createServer(onRequest).listen(8888);
+exports.start = start;
